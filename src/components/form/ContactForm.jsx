b437ab8638fe73e884d49d55bb99e09eb5901a98
sadiq_from_elitebook <sadiqonemail@gmail.com>
2025-04-08 11:34:50 +0530

    fix

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,11 +1,10 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { useToast } from "@/hooks/use-toast";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Checkbox } from "@/components/ui/checkbox";
+import { useToast } from "@/hooks/use-toast";
 
 const services = [
   "NET Development",
